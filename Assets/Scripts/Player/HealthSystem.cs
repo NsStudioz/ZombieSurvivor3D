b65fabc742762c9da3e6c0b63fd0d5011903a95c
NsStudioz <sneve2016@gmail.com>
2023-09-05 14:07:37 +0300

    feat: HealthSystem.cs = Add take health damage for player feature.

--- Assets/Scripts/Player/HealthSystem.cs
+++ Assets/Scripts/Player/HealthSystem.cs
@@ -7,7 +7,9 @@ public class HealthSystem : MonoBehaviour
     [Header("Main Elements")]
     [SerializeField] HealthComponent healthComponent;
 
-
+    [Header("Player Elements")]
+    [SerializeField] float timeElapsed;
+    [SerializeField] float timeElapsedThreshold = 3f;
 
     private void Start()
     {
@@ -15,6 +17,25 @@ public class HealthSystem : MonoBehaviour
         healthComponent = healthComponentInstance;
     }
 
+    private void Update()
+    {
+        //TakeDamageTest();
+        //RegisterPlayerHit();
+        //RegenerateHealth();
+    }
+
+    private void TakeDamageTest()
+    {
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            healthComponent.TakeDamage(1);
+            Debug.Log("Player Health: " + healthComponent.GetCurrentHealth());
+            timeElapsed = timeElapsedThreshold;
+        }
+
+        if (healthComponent.GetCurrentHealth() <= 0)
+            Debug.Log("Player: Died!");
+    }
 
 
 }
