61aa49f516a7a061774f53960e832a48ed87eccc
NsStudioz <sneve2016@gmail.com>
2023-09-05 11:17:28 +0300

    feat: Add Isometric look feature to Player.

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -7,6 +7,12 @@ public class PlayerController : MonoBehaviour
     [Header("Camera Elements")]
     Camera mainCamera;
 
+    [Header("RayCast Elements")]
+    Ray cameraRay;
+    RaycastHit cameraRayHit;
+    [SerializeField] LayerMask rayCastHitLayers;
+    [SerializeField] float rayMaxDistance;
+
     [Header("Main Elements")]
     [SerializeField] float moveSpeed = 1f;
     private const float zeroFloat = 0f;
@@ -30,6 +36,7 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         MovePlayer(Time.deltaTime);
+        Look();
     }
 
     private void MovePlayer(float deltaTime)
@@ -44,5 +51,15 @@ public class PlayerController : MonoBehaviour
         transform.position += upMovement;
     }
     
+    private void Look() // using raycast
+    {
+        cameraRay = mainCamera.ScreenPointToRay(Input.mousePosition);
+
+        if(Physics.Raycast(cameraRay, out cameraRayHit, rayMaxDistance, rayCastHitLayers))
+        {
+            Vector3 targetPos = new Vector3(cameraRayHit.point.x, transform.position.y, cameraRayHit.point.z);
+            transform.LookAt(targetPos);
+        }
+    }
 
 }
