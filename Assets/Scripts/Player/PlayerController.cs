75f24292bcb3a5ad9285f0eb64ca2a14e8e3f7d4
NsStudioz <sneve2016@gmail.com>
2023-09-05 10:11:33 +0300

    feat: Add player movement feature.

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,16 +4,38 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("Camera Elements")]
+    Camera mainCamera = Camera.main;
 
+    [Header("Main Elements")]
+    [SerializeField] float moveSpeed = 1f;
+    private const float zeroFloat = 0f;
+    [SerializeField] Vector3 forward, right;
 
     void Start()
     {
-        
+        forward = mainCamera.transform.forward;
+        forward.y = 0f;
+        forward = Vector3.Normalize(forward);
     }
 
 
     void Update()
     {
-        
+        MovePlayer(Time.deltaTime);
     }
+
+    private void MovePlayer(float deltaTime)
+    {
+        Vector3 rightMovement = right * moveSpeed * deltaTime * Input.GetAxis("Horizontal");
+        Vector3 upMovement = forward * moveSpeed * deltaTime * Input.GetAxis("Vertical");
+
+        Vector3.Normalize(rightMovement);
+        Vector3.Normalize(upMovement);
+
+        transform.position += rightMovement;
+        transform.position += upMovement;
+    }
+    
+
 }
