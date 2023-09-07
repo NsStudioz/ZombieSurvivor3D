7da78031ea5bc98133a657324d092bb28ace37fa
NsStudioz <sneve2016@gmail.com>
2023-09-07 19:38:41 +0300

    fix: Removed monobehaviour from GameStateManager.cs.

--- Assets/Scripts/GameState/GameStateManager.cs
+++ Assets/Scripts/GameState/GameStateManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameStateManager : MonoBehaviour
+public class GameStateManager
 {
     
     private static GameStateManager instance;
