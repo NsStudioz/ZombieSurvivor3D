955f9fee742a827b9f616c578a4fbe0d7ba90348
NsStudioz <sneve2016@gmail.com>
2023-09-11 10:35:14 +0300

    feat: new methods for handheld objects.

--- Assets/Scripts/Interfaces/IHandheldObject.cs
+++ Assets/Scripts/Interfaces/IHandheldObject.cs
@@ -2,17 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class IHandheldObject : MonoBehaviour
+public interface IHandheldObject
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    void OnAttachedToPlayer();
+    void OnEquip();
+    void OnUnequip();
+
 }
