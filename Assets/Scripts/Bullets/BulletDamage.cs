1fae5378deda2aaff40ac481a256d781edb40789
NsStudioz <sneve2016@gmail.com>
2023-09-14 12:43:31 +0300

    refactor: BulletDamage.cs code refactor

--- Assets/Scripts/Bullets/BulletDamage.cs
+++ Assets/Scripts/Bullets/BulletDamage.cs
@@ -5,24 +5,14 @@ using UnityEngine;
 public class BulletDamage : MonoBehaviour
 {
 
-    [SerializeField] int weaponDamage;
-
-    private void Awake()
-    {
-        
-    }
-
-    private void Update()
-    {
-        
-    }
+    [SerializeField] int bulletDamage;
 
     private void OnTriggerEnter(Collider col)
     {
         if (col.CompareTag("Enemy"))
         {
             IDamageable damageable = col.GetComponent<IDamageable>();
-            damageable?.TakeDamage(weaponDamage); // if damageable is not null...Then Damage
+            damageable?.TakeDamage(bulletDamage); // if damageable is not null...Then Damage
             Debug.Log("Enemy Hit!");
             Destroy(gameObject);
         }
