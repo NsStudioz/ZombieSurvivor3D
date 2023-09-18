a991e9f2d92dde348d3cb5bfe9e4a9be815affb0
NsStudioz <sneve2016@gmail.com>
2023-09-18 17:38:38 +0300

    feat: added fields and lists.

--- Assets/Scripts/Enemy/EnemySpawner.cs
+++ Assets/Scripts/Enemy/EnemySpawner.cs
@@ -5,6 +5,23 @@ using UnityEngine;
 public class EnemySpawner : MonoBehaviour
 {
 
+    [Header("Main Elements")]
+    [SerializeField] List<GameObject> enemyStandardPrefabs = new List<GameObject>();
+    [SerializeField] List<Transform> spawnPoints = new List<Transform>();
+
+    [Header("Spawning")]
+    [SerializeField] int pointerIndex = 0;
+    [SerializeField] int intRandomizer;
+    //
+    [SerializeField] float timer;
+    [SerializeField] float timerThreshold;
+
+    [Header("Limits")]
+    [SerializeField] int minInt;
+    [SerializeField] int maxInt;
+    [SerializeField] int minFloat;
+    [SerializeField] int maxFloat;
+
     void Start()
     {
         
