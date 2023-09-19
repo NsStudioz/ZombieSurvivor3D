b2c95fbb70bc7b754ec1e86bd33001a30cbd4345
NsStudioz <sneve2016@gmail.com>
2023-09-19 15:16:42 +0300

    feat: object pool prototype for recycleable objects.

--- Assets/Scripts/ObjectPool/ObjectPool.cs
+++ Assets/Scripts/ObjectPool/ObjectPool.cs
@@ -1,8 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class ObjectPool : MonoBehaviour
 {
-    
+
+    public void SpawnObject(Queue<GameObject> queueGO, int maxQueueCount ,GameObject prefabInstance, Vector3 position, Quaternion rotation)
+    {
+        if(queueGO.Count < maxQueueCount)
+        {
+            GameObject newObjectInstance = Instantiate(prefabInstance, position, rotation);
+            // add to EnemyMaxCount
+        }
+        else
+            PopObjectFromPool(queueGO, position, rotation);
+    }
+
+    private static void PopObjectFromPool(Queue<GameObject> queueGO, Vector3 position, Quaternion rotation)
+    {
+        GameObject existingPrefabInstance = queueGO.Dequeue();
+        existingPrefabInstance.SetActive(true);
+        SetObjectsPositionAndRotation(existingPrefabInstance, position, rotation);
+    }
+
+    private static void SetObjectsPositionAndRotation(GameObject instance, Vector3 position, Quaternion rotation)
+    {
+        instance.transform.position = position;
+        instance.transform.rotation = rotation;
+    }
+
+    public void DespawnObjectDelay(float timeDelay, Queue<GameObject> queueGO, GameObject instanceToDespawn, Vector3 position, Quaternion rotation)
+    {
+        StartCoroutine(DespawnObjectCoroutine(timeDelay, queueGO, instanceToDespawn, position, rotation));
+    }
+
+    public void DespawnObjectImmediately(Queue<GameObject> queueGO, GameObject instanceToDespawn, Vector3 position, Quaternion rotation)
+    {
+        DespawnObject(queueGO, instanceToDespawn, position, rotation);
+    }
+
+    private IEnumerator DespawnObjectCoroutine(float timeDelay, Queue<GameObject> queueGO, GameObject instanceToDespawn, Vector3 position, Quaternion rotation)
+    {
+        yield return new WaitForSeconds(timeDelay);
+        DespawnObject(queueGO, instanceToDespawn, position, rotation);
+    }
+
+    private void DespawnObject(Queue<GameObject> queueGO, GameObject instanceToDespawn, Vector3 position, Quaternion rotation)
+    {
+        queueGO.Enqueue(instanceToDespawn);
+        instanceToDespawn.SetActive(false);
+        SetObjectsPositionAndRotation(instanceToDespawn, position, rotation);
+    }
+
+
+
 }
