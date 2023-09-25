7f3e01ecbf16315018c2162d4116bc38a9000e7f
NsStudioz <sneve2016@gmail.com>
2023-09-25 11:33:09 +0300

    feat: ScoreComponent.cs = added helper functions

--- Assets/Scripts/Components/ScoreComponent.cs
+++ Assets/Scripts/Components/ScoreComponent.cs
@@ -2,15 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
 public struct ScoreComponent 
 {
 
     public int highScore;
 
+    public ScoreComponent(int initValue)
+    {
+        highScore = initValue;
+    }
 
     public int GetUpdatedHighScore(int scorePoints)
     {
         return highScore + scorePoints;
+    }
+
+    public void UpdateScore(int scorePoints)
+    {
+        highScore = GetUpdatedHighScore(scorePoints);
     }
 
 }
