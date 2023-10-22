d8dea90d95159e9d6a7df23d28dd542f93f42738
NsStudioz <sneve2016@gmail.com>
2023-10-22 18:52:22 +0300

    new: addressables script.

---
+++ Assets/Scripts/Gameplay/SpawnObjectsAddressables.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ZombieSurvivor3D
+{
+    public class SpawnObjectsAddressables : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
