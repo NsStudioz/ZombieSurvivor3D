81602856f08a7ec7668cc574fba9dbdb4fafefc7
NsStudioz <sneve2016@gmail.com>
2023-10-22 19:06:31 +0300

    feat: addressables spawner feature added.

--- Assets/Scripts/Gameplay/SpawnObjectsAddressables.cs
+++ Assets/Scripts/Gameplay/SpawnObjectsAddressables.cs
@@ -1,21 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
 
 namespace ZombieSurvivor3D
 {
     public class SpawnObjectsAddressables : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
-        {
-        
-        }
 
-        // Update is called once per frame
+        [SerializeField] private AssetLabelReference assetLabelReference;
+
+
         void Update()
         {
-        
+            if (Input.GetKeyDown(KeyCode.T))
+            {
+                Addressables.LoadAssetAsync<GameObject>(assetLabelReference).Completed +=
+                    (AsyncOperationHandle) =>
+                    {
+                        if (AsyncOperationHandle.Status == AsyncOperationStatus.Succeeded)
+                            Instantiate(AsyncOperationHandle.Result);
+                        else
+                            Debug.Log("Failed to load!");
+                    }; 
+            }
         }
     }
 }
