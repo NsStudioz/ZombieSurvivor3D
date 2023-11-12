c8a3b24434fef12a3ffe187017df904263e614aa
NsStudioz <sneve2016@gmail.com>
2023-11-12 16:37:25 +0200

    feat: BlockadeDetector.cs = Pause/Resume feature added.

--- Assets/Scripts/Gameplay/Blockades/BlockadeDetector.cs
+++ Assets/Scripts/Gameplay/Blockades/BlockadeDetector.cs
@@ -17,6 +17,24 @@ namespace ZombieSurvivor3D
 
         private const string BLOCKADE_TAG = "Blockade";
 
+        #region Event Listeners:
+
+        private void Awake()
+        {
+            GameStateManager.Instance.OnGameStateChanged += OnGameStateChanged;
+        }
+
+        private void OnDestroy()
+        {
+            GameStateManager.Instance.OnGameStateChanged -= OnGameStateChanged;
+        }
+
+        private void OnGameStateChanged(GameStateManager.GameState newGameState)
+        {
+            enabled = newGameState == GameStateManager.GameState.Gameplay;
+        }
+
+        #endregion
 
         private void OnTriggerEnter(Collider col)
         {
