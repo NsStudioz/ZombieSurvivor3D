fe53e91544bc6438e0717bafe8a8dddeee2c7237
NsStudioz <sneve2016@gmail.com>
2023-11-12 17:04:31 +0200

    feat: LootRandomizerSystem.cs = Pause/Resume feature added.

--- Assets/Scripts/Gameplay/RNG/LootRandomizerSystem.cs
+++ Assets/Scripts/Gameplay/RNG/LootRandomizerSystem.cs
@@ -1,12 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using ZombieSurvivor3D.Gameplay.GameState;
 
 namespace ZombieSurvivor3D
 {
     public class LootRandomizerSystem : MonoBehaviour
     {
 
+        #region EventListeners:
+
+        private void Awake()
+        {
+            GameStateManager.Instance.OnGameStateChanged += OnGameStateChanged;
+        }
+
+        private void OnDestroy()
+        {
+            GameStateManager.Instance.OnGameStateChanged -= OnGameStateChanged;
+        }
+
+        private void OnGameStateChanged(GameStateManager.GameState newGameState)
+        {
+            enabled = newGameState == GameStateManager.GameState.Gameplay;
+        }
+
+        #endregion
 
     }
 }
