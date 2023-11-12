174152d2c232607fa28eed55a4296964874d21f4
NsStudioz <sneve2016@gmail.com>
2023-11-12 15:56:40 +0200

    [ 8.11.2023 - 20:00:00 PM ] feat: Blockade detection and clearing system implemented.

---
+++ Assets/Scripts/Gameplay/Blockades/BlockadeDetector.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Rendering.LookDev;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using ZombieSurvivor3D.Blockades;
+using ZombieSurvivor3D.Gameplay.GameState;
+using ZombieSurvivor3D.Gameplay.Score;
+
+namespace ZombieSurvivor3D
+{
+    public class BlockadeDetector : MonoBehaviour, ControlsPC.IInteractionControlsActions
+    {
+
+        [SerializeField] private Blockade BlockadeObject = null;
+        [SerializeField] private bool isPressed;
+
+        private const string BLOCKADE_TAG = "Blockade";
+
+
+        private void OnTriggerEnter(Collider col)
+        {
+            if (col.CompareTag(BLOCKADE_TAG))
+                BlockadeObject = col.GetComponent<Blockade>();
+        }
+
+        private void OnTriggerExit(Collider col)
+        {
+            if (col.CompareTag(BLOCKADE_TAG))
+                BlockadeObject = null;
+        }
+
+        private void OpenBlockade()
+        {
+            ScoreSystem.Instance.UpdateScore(BlockadeObject.GetPointCost());
+            BlockadeObject.Destroy();
+            BlockadeObject = null;
+        }
+
+        public void OnInteract(InputAction.CallbackContext context)
+        {
+            if (BlockadeObject == null)
+                return;
+
+            if (context.performed)
+                isPressed = true;
+
+            else if (context.canceled)
+                isPressed = false;
+
+            if (isPressed)
+            {
+                OpenBlockade();
+                isPressed = false;
+            }
+        }
+    }
+}
