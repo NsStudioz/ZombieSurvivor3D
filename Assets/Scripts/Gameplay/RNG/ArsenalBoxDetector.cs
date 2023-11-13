88054d2d64d4340cd6d3c70921cec72712ecf8a6
NsStudioz <sneve2016@gmail.com>
2023-11-13 18:44:06 +0200

    feat: Player arsenal box detection system implemented.

---
+++ Assets/Scripts/Gameplay/RNG/ArsenalBoxDetector.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using ZombieSurvivor3D.Blockades;
+using ZombieSurvivor3D.Gameplay.Score;
+
+namespace ZombieSurvivor3D
+{
+    public class ArsenalBoxDetector : MonoBehaviour, ControlsPC.IInteractionControlsActions
+    {
+
+        [SerializeField] private ArsenalBox ArsenalBoxObject = null;
+        [SerializeField] private bool isPressed;
+
+        private const string ARSENALBOX_TAG = "ArsenalBox";
+
+
+        private void OnTriggerEnter(Collider col)
+        {
+            if (col.CompareTag(ARSENALBOX_TAG))
+                ArsenalBoxObject = col.GetComponent<ArsenalBox>();
+        }
+
+        private void OnTriggerExit(Collider col)
+        {
+            if (col.CompareTag(ARSENALBOX_TAG))
+                ArsenalBoxObject = null;
+        }
+
+        public void OnInteract(InputAction.CallbackContext context)
+        {
+            if (ArsenalBoxObject == null)
+                return;
+
+            if (context.performed)
+                isPressed = true;
+
+            else if (context.canceled)
+                isPressed = false;
+
+            if (isPressed)
+            {
+                OpenInteractableArsenalBox();
+                isPressed = false;
+            }
+        }
+
+        private void OpenInteractableArsenalBox()
+        {
+            ScoreSystem.Instance.UpdateScore(ArsenalBoxObject.GetPointsCost());
+            ArsenalBoxObject.OpenArsenalBoxForLoot();
+        }
+    }
+}
