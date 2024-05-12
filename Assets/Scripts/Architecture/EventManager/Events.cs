70ba0af5ce9da98d0e6e8d4e1bcd8cd95dc7a4e4
NsStudioz <sneve2016@gmail.com>
2024-05-12 15:37:03 +0300

    feat: Event Manager to organize and control all events.

---
+++ Assets/Scripts/Architecture/EventManager/Events.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ZombieSurvivor3D
+{
+    public static class Events
+    {
+        public enum EventKey
+        {
+            None = 0,
+            OnGameState = 1,
+
+        }
+
+    }
+}
